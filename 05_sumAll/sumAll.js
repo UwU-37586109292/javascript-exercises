b404a3d2aae536b046580c56fc56c8593260f770
Zuzka <kikucisko@gmail.com>
2022-07-22 04:37:24 +0200

    sum all numbers between 2 arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+    let sum = 0;
+    let numbers = [number1, number2].sort();
+    if (numbers[0] < 0 || typeof number1 != "number" || typeof number2 != "number") {
+        return 'ERROR';
+    }
 
+    for (let i = numbers[0]; i <= numbers[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
