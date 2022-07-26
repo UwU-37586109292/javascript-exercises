cd9db7da7969ce537a8aa6969e765736af4ddf68
Zuzka <kikucisko@gmail.com>
2022-07-26 20:14:12 +0200

    resolve calculator excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return parseInt(num1) + parseInt(num2);
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return parseInt(num1) - parseInt(num2);
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let runningSum = 0;
+  array.forEach(elem => {
+    runningSum += parseInt(elem);
+  }
+  )
+  return runningSum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let runningResult = 1;
+  array.forEach(elem => {
+    runningResult *= parseInt(elem);
+  }
+  )
+  return runningResult;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(parseInt(num1), parseInt(num2));
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  number = parseInt(number);
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i
+  }
+  return result;
 };
 
 // Do not edit below this line
