bd291bc1a8a213d81081045d37210d978a8493f2
Zuzka <kikucisko@gmail.com>
2022-07-27 09:58:00 +0200

    Solve fibonacci excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    number = parseInt(number);
+    let fibCurrent = 1;
+    let fibPrev = 1;
+    let temp;
+    if (number > 0) {
+        for (let i = 1; i <= number - 2; i++) {
+            temp = fibCurrent;
+            fibCurrent = fibCurrent + fibPrev;
+            fibPrev = temp;
+        }
+        return fibCurrent;
+    }
+    else return "OOPS";
 };
 
 // Do not edit below this line
