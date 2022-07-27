99a18b74243a1170db9bd4d5a389a2d5c2122395
Zuzka <kikucisko@gmail.com>
2022-07-27 09:46:34 +0200

    Solve palindrome excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let chars = /\s*\.*\,*\!*/g;
+    string = string.replace(chars, '');
+    stringReverse = string.split('').reverse().join('');
+    console.log(string);
+    console.log(stringReverse);
+    return string === stringReverse;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
