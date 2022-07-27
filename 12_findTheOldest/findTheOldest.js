fc8c5482277acb6ee92bf44d0a598d53bb2bba19
Zuzka <kikucisko@gmail.com>
2022-07-27 15:17:43 +0200

    resolve

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleList) {
+
+    return peopleList.reduce((oldestPerson, currentPerson) => {
+        let oldestPersonAge = calculateAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        let currentPersonAge = calculateAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (oldestPersonAge < currentPersonAge) {
+            return currentPerson;
+        } else {
+            return oldestPerson;
+        }
+
+    });
+
 
 };
-
+const calculateAge = function (yob, yod) {
+    if (!yod) {
+        yod = new Date().getFullYear();
+    }
+    return yod - yob;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
